7928bfab26e9278b0049154d9108128fc02bf582
Rui Vergani Neto <ruiverganineto@gmail.com>
2021-12-02 12:14:09 +0000

    data structure done

--- js/scripts.js
+++ js/scripts.js
@@ -10,3 +10,36 @@ const vendido = false; // nao altera a variavel
 console.log(marcaCarro);
 console.log(anoCarro);
 
+// Data Structure
+
+var carro = 'Gol'; // String
+var anoCarro2 = 2021; // Number
+var carroVendido = true; // Boolean
+var qtdRodas; // Undefined
+var carroAutomatico; // Null
+var Simbolo = Symbol(); // Symbol
+var ObjetoCarro = { // Object
+    nome: 'Gol',
+    anoFabricacao: 2021,
+    marca: 'Volswagen'
+}; 
+console.log(ObjetoCarro);
+console.log(ObjetoCarro.nome);
+var objetoPosicoes = [ // Object (more positions)
+    { 
+        nome: 'Gol',
+        anoFabricacao: 2021,
+        marca: 'Volswagen'
+    },
+    { 
+        nome: 'Polo',
+        anoFabricacao: 2021,
+        marca: 'Volswagen'
+    },
+];
+console.log(objetoPosicoes[0].nome);
+console.log(objetoPosicoes.length);
+
+console.log(typeof carro); // Type of Variable
+console.log("O carro com nome " + carro + " foi fabricado no ano de " + anoCarro2); // Concatenacao
+console.log(`O nome do carro e ${carro}`); // Template String (ECMA Script 6)
