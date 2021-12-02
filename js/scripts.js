002ff59480ea783684ae5827a1e0a3be8fcb0904
Rui Vergani Neto <ruiverganineto@gmail.com>
2021-12-02 11:34:14 +0000

    variables JS

---
+++ js/scripts.js
@@ -0,0 +1,12 @@
+console.log('Hello World!!'); // escreve no console do navegador
+// alert('Isto e um alert do javascript'); // manda um alerta para o navegador
+
+
+// Variables in JS
+var marcaCarro = 'Chevrolet';
+let anoCarro = 2021;
+const vendido = false; // nao altera a variavel
+
+console.log(marcaCarro);
+console.log(anoCarro);
+
