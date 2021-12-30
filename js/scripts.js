77a68b8aad83cc163f4c334415fa67fab90ce8a1
Rui Vergani Neto <ruiverganineto@gmail.com>
2021-12-30 21:28:20 +0000

    Operators and numbers

--- js/scripts.js
+++ js/scripts.js
@@ -57,3 +57,38 @@ let medida = 'litros'; // NaN (not a number - if you sum number + string)
 let incrementar = 10;
 console.log(incrementar++) // after
 console.log(++incrementar) // before
+
+// Conditional IF and Else
+if(carroVendido){
+    console.log('O carro foi vendido! ');
+}
+else{
+    console.log('O carro nao foi vendido.');
+}
+
+let possuiCarro = true
+let possuiMoto = false
+if(possuiMoto){
+    console.log('Ele tem uma moto.');
+} 
+else if(possuiCarro){
+    console.log('Ele tem um carro.');
+}
+else{
+    console.log('Ele nao tem nenhum veiculo.');
+}
+
+// False values
+if(false);
+if(0);
+if(NaN);
+if(null);
+if(undefined);
+if('');
+
+// Negative Operator
+if(!true) // false
+if(!'') // true
+if(!undefined); //true
+
+true && true; // AND Operator
