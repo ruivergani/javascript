b54ce56641131b5c7c4bd50fccb6e6b1fda3fc0a
Rui Vergani Neto <ruiverganineto@gmail.com>
2021-12-30 20:34:03 +0000

    Numbers and Operators

--- js/scripts.js
+++ js/scripts.js
@@ -43,3 +43,17 @@ console.log(objetoPosicoes.length);
 console.log(typeof carro); // Type of Variable
 console.log("O carro com nome " + carro + " foi fabricado no ano de " + anoCarro2); // Concatenacao
 console.log(`O nome do carro e ${carro}`); // Template String (ECMA Script 6)
+
+// Numbers and Operators
+let pi = 3.14
+let soma = 5 + 10;
+let subtracao = 10 - 5;
+let multiplicacao = 10 * 5;
+let divisao = 10 / 2;
+let expoente = 2 ** 4;
+let medida = 'litros'; // NaN (not a number - if you sum number + string)
+
+// Unario Operator
+let incrementar = 10;
+console.log(incrementar++) // after
+console.log(++incrementar) // before
