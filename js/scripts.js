0291b8249508221adab3ca4f20206476127b5136
Rui Vergani Neto <ruiverganineto@gmail.com>
2022-01-03 19:01:01 +0000

    loop with array

--- js/scripts.js
+++ js/scripts.js
@@ -130,11 +130,22 @@ console.log(filhos); // print array
 
 filhos.pop(); // Remove last item of array
 filhos.push('Cruze'); // Add in the end of array
-filhos.length(); // 4
+filhos.length; // 4
 
 // For loop
-for(var numero = 0; numero < 10; numero++){
+for(var numero = 0; numero <= 10; numero++){
     console.log(numero);
 }
 
-// retorna de 0 a 9 no console
+// While
+var i = 0;
+while(i < 10){
+    console.log(i);
+    i++;
+}
+
+// Loops com Array
+var testing = ['Fusca', 'Ferrari', 'Gol'];
+for(var i = 0; i < testing.length; i++){
+    console.log(testing[i])
+}
