1a6e54bae8653f348915302d32058a5fd6d6a816
Rui Vergani Neto <ruiverganineto@gmail.com>
2022-01-26 20:19:34 +0000

    Buttons exercise

--- js/scripts.js
+++ js/scripts.js
@@ -231,3 +231,16 @@ console.log(image.getAttribute('src')); // pegar atributo src da imagem
 
 // setAttribute
 console.log(image.setAttribute('src', '/caminho-da-imagem')); // colocar um atributo na imagem
+
+// Change password to view/not
+const button = document.querySelector('button')
+const input = document.querySelector('input')
+
+button.addEventListener('click', () => { // no clique a funcao vai ser executada
+    if(input.getAttribute('type') === 'password'){
+        input.setAttribute('type', 'text')
+    }
+    else{
+        input.setAttribute('type', 'password')
+    }
+})
