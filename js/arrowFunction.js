f5ad64ceecf11728ae5e36caa6b7985f88a6c2f1
Rui Vergani Neto <ruiverganineto@gmail.com>
2022-03-11 18:47:49 +0000

    functions expressions

--- js/arrowFunction.js
+++ js/arrowFunction.js
@@ -16,3 +16,29 @@ const x = () =>{
 }
 // check for parameters
 let difference = (a, b) => b ? a - b : "Missing value"; // ternary operator
+
+// use arguments objects
+let showArgs = function(){
+    for(let i = 0; i < arguments.length; i++){
+        console.log(arguments[i]);
+    }
+    // useful for a variable (unknown) number of values
+}
+
+// Scope concept
+let age = 20;
+if (age > 18){
+    var drink = true;
+    let vote = true;
+    console.log("Can i drink?", drink);
+    console.log("Can vote?", vote);
+}
+console.log("Can I drink?", drink);
+console.log("Can I vote?", vote); // block level 
+
+// var are function scoped
+// let and const are block scoped (only available where declared)
+
+
+// function can be declared anywhere and called BEFORE or AFTER declaration excepet when using functions expressions or arrows
+
