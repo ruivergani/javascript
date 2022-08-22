bd47efbc27584b332c8d2ed7d2c31ae109376564
Rui Vergani Neto <ruiverganineto@gmail.com>
2022-08-22 19:44:54 +0100

    instructions file

---
+++ js/jon_duckett.js
@@ -0,0 +1,13 @@
+// This page is about the book and concepts of JavaScript
+
+/* 
+
+1. Access the content of the page
+2. Modify the conent of the page
+3. Program rules the browser can follow
+4. React to events triggered by the user or browser
+
+
+
+
+*/
