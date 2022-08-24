39225b3276a02896ed682472783906b7d0e06bcc
Rui Vergani Neto <ruiverganineto@gmail.com>
2022-08-24 19:07:34 +0100

    chapter 1

--- js/jon_duckett.js
+++ js/jon_duckett.js
@@ -6,8 +6,20 @@
 2. Modify the conent of the page
 3. Program rules the browser can follow
 4. React to events triggered by the user or browser
+5. Make use of flowcharts to design the tasks
 
+Object {
+    Properties: characteristics
+    Events: interaction with objects
+    Methods: things people need to do with objects
+}
 
-
-
+1. Chapter:
+    - Learning using Window/Document Object
+    - No need to specify what type data the variable holds
+    - Naming rules: start a letter, $, _ | dont use dash or dot
+    - CaseSensitive
+    - Array: usually a list that you dont know how many items you need
+    - Synchronous: uma tarefa e concluida apos a outra
 */
+
